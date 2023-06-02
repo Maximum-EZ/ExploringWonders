4dedfb4b81612f9eb046af0466a270ca87fa7f45
LiamSirBean <rfwrl13@gmail.com>
2023-06-02 18:42:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const routes = [
   { path: "/", file: "index.html" },
   { path: "/nutrition", file: "/articles/nutrition.html" },
   { path: "/ancient", file: "/articles/ancient.html" },
+  { path: "/maldives", file: "/articles/maldives.html" },
+  { path: "/grand-canyon", file: "/articles/grand-canyon.html" },
+  { path: "/bali", file: "/articles/bali.html" },
+  { path: "/petra", file: "/articles/petra.html" },
+  { path: "/iceland", file: "/articles/iceland.html" },
+  { path: "/serengeti", file: "/articles/serengeti.html" },
+  { path: "/angkor", file: "/articles/angkor.html" },
+  { path: "/tokyo", file: "/articles/tokyo.html" },
   { path: "/404", file: "404.html" },
 ];
 
