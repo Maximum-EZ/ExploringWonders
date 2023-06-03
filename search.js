4cb21d23b614314428205bccc53b9f077d6dedd6
LiamSirBean <rfwrl13@gmail.com>
2023-06-03 16:27:33 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,35 +1,35 @@
-// Get references to the search input and button
-const searchForm = document.querySelector(".search-form");
-const searchInput = document.querySelector(".search-input");
-const searchResults = document.getElementById("search-results");
+// Get references to the search input, search form, and search results container
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-input');
+const searchResults = document.getElementById('search-results');
+const articlesContainer = document.querySelector('.articles');
 
 // Add event listener to the search form
-searchForm.addEventListener("submit", function (event) {
+searchForm.addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent form submission
 
   const searchQuery = searchInput.value.toLowerCase(); // Get the search query and convert it to lowercase
-  const articles = document.querySelectorAll(".articles .article"); // Get all the articles
+  const articles = articlesContainer.querySelectorAll('.article'); // Get all the articles
 
   // Clear previous search results
-  searchResults.innerHTML = "";
+  searchResults.innerHTML = '';
 
   // Loop through the articles and check if they match the search query
-  for (let i = 0; i < articles.length; i++) {
-    const article = articles[i];
-    const title = article.querySelector(".title").innerText.toLowerCase();
-    const content = article.querySelector(".content").innerText.toLowerCase();
+  articles.forEach(function (article) {
+    const title = article.querySelector('.title').innerText.toLowerCase();
+    const content = article.querySelector('.content').innerText.toLowerCase();
 
     // Check if the title or content contains the search query
     if (title.includes(searchQuery) || content.includes(searchQuery)) {
       // Show the article if it matches the search query
-      article.style.display = "block";
+      article.style.display = 'block';
 
       // Create a new result element and append it to the search results
-      const result = document.createElement("div");
+      const result = document.createElement('div');
       result.innerText = title;
       searchResults.appendChild(result);
     } else {
-      article.style.display = "none"; // Hide the article if it doesn't match the search query
+      article.style.display = 'none'; // Hide the article if it doesn't match the search query
     }
-  }
+  });
 });
