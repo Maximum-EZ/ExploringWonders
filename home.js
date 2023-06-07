0ab5e495f721110faefc78835266ab3ed047ca46
LiamSirBean <rfwrl13@gmail.com>
2023-06-07 16:18:25 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var headings = document.getElementsByTagName('h1');
+    for (var i = 0; i < headings.length; i++) {
+      headings[i].addEventListener('click', function() {
+        window.location.href = 'index.html'; // Replace with your desired index page URL
+      });
+    }
+  });
+  
