6980cc878d6351fc29ab1b182dabefde15a3c16a
LiamSirBean <rfwrl13@gmail.com>
2023-06-07 16:22:12 -0400

    Fixes!

--- home.js
+++ home.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var headings = document.getElementsByTagName('h1');
-    for (var i = 0; i < headings.length; i++) {
-      headings[i].addEventListener('click', function() {
-        window.location.href = 'index.html'; // Replace with your desired index page URL
-      });
-    }
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  var headings = document.getElementsByTagName("h1");
+  for (var i = 0; i < headings.length; i++) {
+    headings[i].addEventListener("click", function () {
+      window.location.href = "index.html"; // Replace with your desired index page URL
+    });
+  }
+});
