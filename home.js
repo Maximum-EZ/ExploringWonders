63951a1957ddbda67409ea602d0c4770de9dc3f4
LiamSirBean <rfwrl13@gmail.com>
2023-06-14 16:35:48 -0400

    Format

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var headings = document.getElementsByTagName("h1");
   for (var i = 0; i < headings.length; i++) {
     headings[i].addEventListener("click", function () {
-      window.location.href = "/index.html"; 
+      window.location.href = "/index.html";
     });
   }
 });
