8d2b3b2db85383bc6364c808200646fff63b0c2d
LiamSirBean <rfwrl13@gmail.com>
2023-06-17 13:56:45 -0400

    Massive Update

--- index.js
+++ index.js
@@ -7,22 +7,8 @@ const app = express();
 
 const routes = [
   { path: "/", file: "index.html" },
-  { path: "/ancient", file: "/articles/ancient.html" },
-  { path: "/technology", file: "/articles/technology.html" },
-  { path: "/grand-canyon", file: "/articles/grand-canyon.html" },
-  { path: "/bali", file: "/articles/bali.html" },
-  { path: "/petra", file: "/articles/petra.html" },
-  { path: "/iceland", file: "/articles/iceland.html" },
-  { path: "/serengeti", file: "/articles/serengeti.html" },
-  { path: "/angkor", file: "/articles/angkor.html" },
-  { path: "/amalfi-coast", file: "/articles/amalfi-coast.html" },
-  { path: "/istanbul", file: "/articles/istanbul.html" },
-  { path: "/great-barrier-reef", file: "/articles/great-barrier-reef.html" },
-  { path: "/stonehenge", file: "/articles/stonehenge.html" },
-  { path: "/tea-ceremony", file: "/articles/tea-ceremony.html" },
-  { path: "/amazon-rainforest", file: "/articles/amazon-rainforest.html" },
-  { path: "/italian", file: "/articles/italian.html" },
-  { path: "/privacy", file: "/privacy.html" },
+  { path: "/about", file: "/articles/about.html" },
+  { path: "/policy", file: "/policy.html" },
   { path: "/404", file: "404.html" },
 ];
 
